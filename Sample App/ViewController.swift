3a5ac2c9bd148c8fd707cc891c0883264cfdc57e
Jelzon Monzon <jelzon@livelikevr.com>
2020-04-03 12:19:58 -0400

    adds empty xcode project

---
+++ Sample App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample App
+//
+//  Created by Jelzon WORK on 4/3/20.
+//  Copyright © 2020 LiveLike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
