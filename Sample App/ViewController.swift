6687964bf5ae6ff116a6938b8859b368f3eb3b61
Jelzon Monzon <jelzon@livelikevr.com>
2020-04-03 17:40:22 -0400

    adds client id and program id text field to the main view controller

--- Sample App/ViewController.swift
+++ Sample App/ViewController.swift
@@ -7,12 +7,84 @@
 //
 
 import UIKit
+import EngagementSDK
 
 class ViewController: UIViewController {
-
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private let sdkVersionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "LL SDK [\(EngagementSDK.version)]"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let clientIDLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Client ID"
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private let clientIDTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter Client ID"
+        return textField
+    }()
+    
+    private let programIDLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Program ID"
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private let programIDTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter Program ID"
+        return textField
+    }()
+    
+    private let useCasesLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Use Cases"
+        label.textAlignment = .left
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+        ])
+        
+        stackView.addArrangedSubview(sdkVersionLabel)
+        stackView.addArrangedSubview(clientIDLabel)
+        stackView.addArrangedSubview(clientIDTextField)
+        stackView.addArrangedSubview(programIDLabel)
+        stackView.addArrangedSubview(programIDTextField)
+        stackView.addArrangedSubview(useCasesLabel)
     }
 
 
